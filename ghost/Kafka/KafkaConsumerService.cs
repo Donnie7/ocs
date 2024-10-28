d42554878faaeec3458f74687c2b272c68511d74
Gabriel Calmeiro <gabriel.calmeiro@farfetch.com>
2024-10-28 22:19:24 +0000

    Create the first iteration of KafkaConsumer

---
+++ ghost/Kafka/KafkaConsumerService.cs
@@ -0,0 +1,37 @@
+﻿namespace ghost.Kafka;
+
+using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+
+public class KafkaConsumerService : BackgroundService
+{
+    private const string Topic = "command-bus";
+    
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            GroupId = "ghost-consumer",
+            EnableAutoOffsetStore = false,
+            EnableAutoCommit = true
+        };
+
+        using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+        {
+            consumer.Subscribe(Topic);
+
+            //while (!canceled)
+            while (true)
+            {
+                var consumeResult = consumer.Consume(stoppingToken);
+                Console.WriteLine(consumeResult.Message);
+                
+                // process message here
+                consumer.StoreOffset(consumeResult);
+            }
+            
+            //consumer.Close();
+        }
+    }
+}
--- ghost/Program.cs
+++ ghost/Program.cs
@@ -1,4 +1,37 @@
-﻿using OpenQA.Selenium;
+﻿namespace ghost;
+
+using Kafka;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    static async Task Main()
+    {
+        var host = CreateHostBuilder().Build();
+
+        await host.RunAsync();
+    }
+
+    public static IHostBuilder CreateHostBuilder() =>
+        Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+                {
+                    services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+                    services.AddHostedService<KafkaConsumerService>();
+                })
+            .ConfigureAppConfiguration((builderContext, config) =>
+            {
+                var env = builderContext.HostingEnvironment;
+                config
+                    .AddJsonFile("conf/appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"conf/appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables();
+            });
+}
+/*
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 var driver = new ChromeDriver();
@@ -10,3 +43,4 @@ driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/div[2]/div/input")).SendKeys
 driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/div[3]/div/input")).SendKeys("naosei_534");
 driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/p/button[1]")).Click();
 driver.Quit();
+*/
