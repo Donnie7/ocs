91ae8c47bf403da80094c76db53ea2174ed7ef27
Gabriel Calmeiro <gabriel.calmeiro@farfetch.com>
2024-10-28 21:10:04 +0000

    Add web-reach proj
    A browser interation tool

--- ghost/Program.cs
+++ ghost/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
-Console.WriteLine("Hello, World!");
+var driver = new ChromeDriver();
+driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+driver.Url = "https://lobby.ogame.gameforge.com/pt_PT/";
+var element = driver.FindElement(By.XPath("//*[@id=\"loginRegisterTabs\"]/ul/li[1]"));
+element.Click();
+driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/div[2]/div/input")).SendKeys("alt.mail.16@gmail.com");
+driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/div[3]/div/input")).SendKeys("naosei_534");
+driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/p/button[1]")).Click();
+driver.Quit();
