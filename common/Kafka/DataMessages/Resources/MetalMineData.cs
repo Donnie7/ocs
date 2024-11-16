ca6f92db150113a8b3b113a08e5a1584d808c8f1
Gabriel Calmeiro <gabriel.calmeiro@farfetch.com>
2024-11-16 23:39:26 +0000

    Add more stuff in ToDoList

--- common/Kafka/DataMessages/Resources/MetalMineData.cs
+++ common/Kafka/DataMessages/Resources/MetalMineData.cs
@@ -1,7 +1,12 @@
 ﻿namespace common.Kafka.DataMessages.Resources;
 
+using MessagePack;
+
+[MessagePackObject]
 public class MetalMineData : GlobalData
 {
+    [Key(0)]
     public int Level { get; set; }
+    [Key(1)]
     public bool IsUpgrading { get; set; } 
 }
--- ghost/CommandHandler/Navigation/ResourcesCommandHandler.cs
+++ ghost/CommandHandler/Navigation/ResourcesCommandHandler.cs
@@ -1,20 +1,53 @@
 ﻿namespace ghost.CommandHandler.Navigation;
 
+using common.DataFillers;
+using common.Domain;
 using common.Kafka.Commands.Navigation;
+using Kafka.Producer;
 using MediatR;
+using web_reach.DataCollector.Resources;
 using web_reach.Interfaces;
 
 public class ResourcesCommandHandler : IRequestHandler<ResourcesCommand>
 {
     private readonly INavigationCommands navigationCommands;
-    
-    public ResourcesCommandHandler(INavigationCommands navigationCommands)
+    private readonly Account account;
+    private readonly MetalMineCollector metalMineCollector;
+    private readonly CrystalMineCollector crystalMineCollector;
+    private readonly DataCollectorProducer dataCollectorProducer;
+
+    public ResourcesCommandHandler(
+        INavigationCommands navigationCommands,
+        Account account,
+        MetalMineCollector metalMineCollector,
+        CrystalMineCollector crystalMineCollector,
+        DataCollectorProducer dataCollectorProducer)
     {
         this.navigationCommands = navigationCommands;
+        this.account = account;
+        this.metalMineCollector = metalMineCollector;
+        this.crystalMineCollector = crystalMineCollector;
+        this.dataCollectorProducer = dataCollectorProducer;
     }
     
-    public Task Handle(ResourcesCommand request, CancellationToken cancellationToken)
+    public async Task Handle(ResourcesCommand request, CancellationToken cancellationToken)
     {
-        return navigationCommands.Resources();
+        await navigationCommands.Resources();
+        await CollectMetalMineData();
+        await CollectCrystalMineData();
+    }
+
+    private async Task CollectCrystalMineData()
+    {
+        var crytalMineData = await crystalMineCollector.Collect();
+        GlobalDataFiller.UpdateAccount(account, crytalMineData);
+        await dataCollectorProducer.SendCollectedData(crytalMineData);
+    }
+
+    private async Task CollectMetalMineData()
+    {
+        var metalMineData = await metalMineCollector.Collect();
+        GlobalDataFiller.UpdateAccount(account, metalMineData);
+        await dataCollectorProducer.SendCollectedData(metalMineData);
     }
 }
