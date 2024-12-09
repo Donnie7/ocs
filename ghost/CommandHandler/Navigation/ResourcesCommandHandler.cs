810051bb52f27a2170df404eb61bd0a13ba10213
Gabriel Calmeiro <gabriel.calmeiro@farfetch.com>
2024-12-09 22:11:12 +0000

    Implement all Resource Collectors

--- ghost/CommandHandler/Navigation/ResourcesCommandHandler.cs
+++ ghost/CommandHandler/Navigation/ResourcesCommandHandler.cs
@@ -14,6 +14,12 @@ public class ResourcesCommandHandler : IRequestHandler<ResourcesCommand>
     private readonly Account account;
     private readonly MetalMineCollector metalMineCollector;
     private readonly CrystalMineCollector crystalMineCollector;
+    private readonly DeuteriumSynthesizerCollector deuteriumSynthesizerCollector;
+    private readonly SolarPlantCollector solarPlantCollector;
+    private readonly FusionReactorCollector fusionReactorCollector;
+    private readonly MetalStorageCollector metalStorageCollector;
+    private readonly CrystalStorageCollector crystalStorageCollector;
+    private readonly DeuteriumTankCollector deuteriumTankCollector;
     private readonly DataCollectorProducer dataCollectorProducer;
 
     public ResourcesCommandHandler(
@@ -21,12 +27,23 @@ public class ResourcesCommandHandler : IRequestHandler<ResourcesCommand>
         Account account,
         MetalMineCollector metalMineCollector,
         CrystalMineCollector crystalMineCollector,
+        DeuteriumSynthesizerCollector deuteriumSynthesizerCollector,
+        SolarPlantCollector solarPlantCollector,
+        FusionReactorCollector fusionReactorCollector,
+        MetalStorageCollector metalStorageCollector,
+        CrystalStorageCollector crystalStorageCollector,
+        DeuteriumTankCollector deuteriumTankCollector,
         DataCollectorProducer dataCollectorProducer)
     {
         this.navigationCommands = navigationCommands;
         this.account = account;
         this.metalMineCollector = metalMineCollector;
         this.crystalMineCollector = crystalMineCollector;
+        this.deuteriumSynthesizerCollector = deuteriumSynthesizerCollector;
+        this.solarPlantCollector = solarPlantCollector;
+        this.fusionReactorCollector = fusionReactorCollector;
+        this.metalStorageCollector = metalStorageCollector;
+        this.crystalStorageCollector = crystalStorageCollector;
         this.dataCollectorProducer = dataCollectorProducer;
     }
     
@@ -35,13 +52,19 @@ public class ResourcesCommandHandler : IRequestHandler<ResourcesCommand>
         await navigationCommands.Resources();
         await CollectMetalMineData();
         await CollectCrystalMineData();
+        await CollectDeuteriumSynthesizerData();
+        await CollectSolarPlantData();
+        await CollectFusionReactorData();
+        await CollectMetalStorageData();
+        await CollectCrystalStorageData();
+        await CollectDeuteriumTankData();
     }
 
     private async Task CollectCrystalMineData()
     {
-        var crytalMineData = await crystalMineCollector.Collect();
-        GlobalDataFiller.UpdateAccount(account, crytalMineData);
-        await dataCollectorProducer.SendCollectedData(crytalMineData);
+        var crystalMineData = await crystalMineCollector.Collect();
+        GlobalDataFiller.UpdateAccount(account, crystalMineData);
+        await dataCollectorProducer.SendCollectedData(crystalMineData);
     }
 
     private async Task CollectMetalMineData()
@@ -49,5 +72,47 @@ public class ResourcesCommandHandler : IRequestHandler<ResourcesCommand>
         var metalMineData = await metalMineCollector.Collect();
         GlobalDataFiller.UpdateAccount(account, metalMineData);
         await dataCollectorProducer.SendCollectedData(metalMineData);
+    }
+    
+    private async Task CollectDeuteriumSynthesizerData()
+    {
+        var deuteriumSynthesizerData = await deuteriumSynthesizerCollector.Collect();
+        GlobalDataFiller.UpdateAccount(account, deuteriumSynthesizerData);
+        await dataCollectorProducer.SendCollectedData(deuteriumSynthesizerData);
+    }
+    
+    private async Task CollectSolarPlantData()
+    {
+        var solarPlantData = await solarPlantCollector.Collect();
+        GlobalDataFiller.UpdateAccount(account, solarPlantData);
+        await dataCollectorProducer.SendCollectedData(solarPlantData);
+    }
+    
+    private async Task CollectFusionReactorData()
+    {
+        var fusionReactorData = await fusionReactorCollector.Collect();
+        GlobalDataFiller.UpdateAccount(account, fusionReactorData);
+        await dataCollectorProducer.SendCollectedData(fusionReactorData);
+    }
+    
+    private async Task CollectMetalStorageData()
+    {
+        var metalStorageData = await metalStorageCollector.Collect();
+        GlobalDataFiller.UpdateAccount(account, metalStorageData);
+        await dataCollectorProducer.SendCollectedData(metalStorageData);
+    }
+    
+    private async Task CollectCrystalStorageData()
+    {
+        var crystalStorageData = await crystalStorageCollector.Collect();
+        GlobalDataFiller.UpdateAccount(account, crystalStorageData);
+        await dataCollectorProducer.SendCollectedData(crystalStorageData);
+    }
+    
+    private async Task CollectDeuteriumTankData()
+    {
+        var deuteriumTankData = await deuteriumTankCollector.Collect();
+        GlobalDataFiller.UpdateAccount(account, deuteriumTankData);
+        await dataCollectorProducer.SendCollectedData(deuteriumTankData);
     }
 }
---
+++ web-reach/DataCollector/Resources/DeuteriumTankCollector.cs
@@ -0,0 +1,18 @@
+﻿namespace web_reach.DataCollector.Resources;
+
+using Commands;
+using common.Kafka.DataMessages.Resources;
+using Selenium;
+
+public class DeuteriumTankCollector(ISeleniumWebDriver driver) : SeleniumCommand(driver), IDataCollector<DeuteriumTankData>
+{
+    private const string CurrentLevelXPath = "//*[@id=\"producers\"]/li[10]/span/span[1]/span[1]";
+    private const string IsUpgradingXPath = "//*[@id=\"producers\"]/li[10]/span/span[1]";
+
+    public async Task<DeuteriumTankData> Collect() =>
+        new()
+        {
+            Level = await Driver.ReadInteger(CurrentLevelXPath),
+            IsUpgrading = await Driver.IsPresent(IsUpgradingXPath),
+        };
+}
